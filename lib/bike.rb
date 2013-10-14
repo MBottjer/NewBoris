d6aecfeea9a696e5bbda85cb864e40f94de8e2d4
Will Bell <willbell@gmail.com>
2013-10-14 19:01:28 +0100

    added fix method for Bike class

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,9 +1,8 @@
 class Bike
 
   def initialize
-    @broken = false
+    fix
   end
-
 
 	def broken?
 	  @broken
@@ -11,6 +10,10 @@ class Bike
 
   def break!
     @broken = true
+  end
+
+  def fix
+  	@broken = false
   end
 
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,20 +2,20 @@ require 'bike'
 
 describe Bike do
 
+	let(:bike) {Bike.new}
+
 	it 'is not broken when initially rented' do 
-		bike = Bike.new
 		expect(bike.broken?).to be_false
 	end
 
 	it 'can be broken' do
-		bike = Bike.new
 		bike.break!
 		expect(bike.broken?).to be_true
 	end
 
   it 'can be fixed' do
-    bike = Bike.new
     bike.break!
     bike.fix
+  end
 
 end
